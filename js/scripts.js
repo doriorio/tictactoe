d65d5bca17db70ab57094339d0218b0c58f249aa
doriorio <itsdori@gmail.com>
2019-10-25 09:41:07 -0700

    first message

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,14 @@ console.log('connected')
 
 /*----- constants -----*/ 
 /*----- app's state (variables) -----*/ 
+let turn, winner, board
+
 /*----- cached element references -----*/ 
 /*----- event listeners -----*/ 
 /*----- functions -----*/
+
+board = [
+    [0,0,0]
+    [0,0,0]
+    [0,0,0]
+]
